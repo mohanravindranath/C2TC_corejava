63583b374fffbe36fa9c69d5d68fe1d8a6ef8903
mohan <mohan@192.168.0.7>
2022-09-28 00:57:36 +0530

    First Commit

---
+++ Java_Project/src/com/tns/programs/Student.java
@@ -0,0 +1,13 @@
+package com.tns.programs;
+
+public class Student {
+	Student(){
+		System.out.println("Student object is created");
+	}
+
+	public static void main(String[] args) {
+		Student obj=new Student();
+
+	}
+
+}
